e963464395be0e684c0199479c6962d2f2a8a1dd
maadt <147260993+maadt@users.noreply.github.com>
2024-01-31 14:35:33 +0900

    課題４実装完了
    カテゴリーのみのデータを取得したいのでEntityクラス、Serviceクラス、Mapperインターフェースを追加する必要がある。

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -6,17 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.entity.Category;
 import com.example.entity.Item;
+import com.example.form.ItemForm;
+import com.example.service.CategoryService;
 import com.example.service.ItemService;
 
 @Controller
 public class ItemController {
 	private final ItemService itemService;
+	private final CategoryService categoryService;
 	
 	@Autowired
-	public ItemController(ItemService itemService) {
+	public ItemController(ItemService itemService, CategoryService categoryService) {
 		this.itemService = itemService;
+		this.categoryService = categoryService;
 	}
 	
 	@GetMapping("/index")
@@ -24,5 +30,13 @@ public class ItemController {
 		List<Item> items = this.itemService.findAll();
 		model.addAttribute("items", items);
 		return "index";
+	}
+	
+	//カテゴリーを一覧表示させるためのメソッド
+	@GetMapping("/create")
+	public String showCreate(@ModelAttribute ItemForm itemForm, Model model) {
+		List<Category> categories = this.categoryService.findAll();
+		model.addAttribute("categories", categories);
+		return "create";
 	}
 }
