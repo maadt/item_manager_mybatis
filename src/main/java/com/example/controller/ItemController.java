27196f4096183e589d018b53b34942ddaebb9e69
maadt <147260993+maadt@users.noreply.github.com>
2024-01-31 10:42:49 +0900

    課題２実装完了

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -1,16 +1,31 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.entity.Item;
+import com.example.service.ItemService;
 
 @Controller
 @RequestMapping("/index")	
 public class ItemController {
-
 	
-	@GetMapping
-	public String index() {
-		return "index";
+	private final ItemService itemService;
+
+	@Autowired
+	public ItemController(ItemService itemService) {
+		this.itemService = itemService;
+	}
+	
+    @GetMapping
+	public String index(Model model) {
+	    List<Item> items = this.itemService.findAll();
+	    model.addAttribute("items", items);
+	    return "index";
     }
 }
